65f06b04ed4e0592d149a52bc58307e599bb8e42
fedyna-k <fedyna.kevin@gmail.com>
2024-03-07 10:53:17 +0100

    Added comment on *.plan.sql to explain queries

--- sql/requete1.plan.sql
+++ sql/requete1.plan.sql
@@ -1,5 +1,12 @@
 -- QUERY PLAN
 
-explain query plan select distinct primaryTitle, originalTitle from
+explain query plan
+
+-- Recherche des titres principaux et originaux
+select distinct primaryTitle, originalTitle from
     movies natural join persons natural join characters
+-- dans lesquels Jean Reno a joué
+
+-- Ce critère de recherche peut être optimisé avec
+-- un index sur la colonne "primaryName"
 where primaryName = 'Jean Reno';
--- sql/requete5.plan.sql
+++ sql/requete5.plan.sql
@@ -1,19 +1,29 @@
 -- QUERY PLAN
 
-explain query plan with connusav as (
+explain query plan
+
+-- On stocke dans une table temporaire les personnes
+-- connus avant la sortie d'Avatar
+with connusav as (
     select distinct pid from
         persons natural join principals natural join movies natural join ratings
     where numvotes > 200000 and startyear < 2009
 ),
+-- On stocke dans une table temporaire les personnes
+-- connus après la sortie d'Avatar
 connusap as (
     select distinct pid from
         persons natural join principals natural join movies natural join ratings
     where numvotes > 200000 and startyear > 2009
 ),
+-- Les nouveaux connus sont la différence entre les connus après
+-- et les connus avant Avatar
 connus as (
     select distinct pid from connusap except select distinct pid from connusav
 )
+-- On récupère parmis ces gens, ceux qui ont travaillé sur Avatar
 select primaryName from
      persons natural join principals natural join movies
 where pid in connus and (primaryTitle = 'Avatar')
+-- Le order by ne sert que pour comparer avec MongoDB
 order by primaryName;
